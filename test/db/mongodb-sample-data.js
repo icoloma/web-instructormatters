f44d6243e2e302c03f6894eb907584db109df79b
Ernesto Hernández <ehdez@extrema-sistemas.com>
2012-11-13 15:53:33 +0100

    Mejoramos la carga en la base de datos

--- test/db/mongodb-sample-data.js
+++ test/db/mongodb-sample-data.js
@@ -1,3 +1,19 @@
+/*
+ Admin user data
+*/
+var admin = {
+    email : "ehdez73@gmail.com",
+    name : "Ernesto"
+  };
+
+use instructormatters;
+
+
+db.certificates.drop();
+db.editions.drop();
+db.videos.drop();
+db.users.drop();
+db.courses.drop();
 
 /* Courses*/
 
@@ -7,7 +23,7 @@ db.courses.save({
   duration: '4 days',
   deleted: false,
   description: 'All about HTML5 and CSS3'
-})
+});
 
 db.courses.save({
   uuid: 'core-spring',
@@ -15,7 +31,7 @@ db.courses.save({
   duration: '4 days',
   deleted: false,
   description: 'Spring framework 101'
-})
+});
 
 db.courses.save({
   uuid: 'spring-web',
@@ -23,7 +39,7 @@ db.courses.save({
   duration: '4 days',
   deleted: false,
   description: 'Rich web applications with spring'
-})
+});
 
 db.courses.save({
   uuid: 'eis',
@@ -31,110 +47,75 @@ db.courses.save({
   duration: '4 days',
   deleted: false,
   description: 'Learn all about Enterprise Integration with Spring framework'
-})
+});
 
 
 /* Users*/ 
 
+
 db.users.save({
-    email: 'ehdez73@gmail.com',
-    name: 'Ernesto',
-    oauth: '116308562574216710328',
+    email: admin.email,
+    name: admin.name,
+    aboutMe : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dapibus tincidunt leo, quis pellentesque massa accumsan in. Nunc imperdiet volutpat diam quis euismod. Vivamus sed endrerit orci. Integer aliquet felis et urna vestibulum nec aliquam orci tempor. In vitae sem augue. Morbi luctus ',
     admin: true,
     deleted: false,
-    languages: 'en, es',
-    courses: [],
-    address : "Madrid, Spain",
-    geopoint : { lat : 40.4166909, lng: -3.70034540000006, zoom : 10 }
-})
-
-
-db.users.save({
-    email: 'ehdez@extrema-sistemas.com',
-    name: 'Ernesto (extrema)',
-    certified: true,
-    oauth: '109792900130705825881',
-    admin: false,
-    deleted: false,
+    certificates: ['core-spring','spring-web','eis'],
     courses: ['core-spring','spring-web','eis'],
     languages: 'en, es',
     address : "Tenerife, Santa Cruz de Tenerife, Spain",
     geopoint : { lat : 28.2915637, lng: -16.629130400000008, zoom : 9 }
 
-})
+});
 
-
-db.users.save(
-{   email: 'icoloma@gmail.com',
-    name: 'Ignacio Coloma',
-    certified: false,
-    oauth: '112843640399200525430',
-    admin: false,
-    deleted: false,
-    courses: ['html5-css3','core-spring'],
-    languages: 'en, es',
-    videos: [{ id: 'mzPxo7Y6JyA',
-                url: 'http://www.youtube.com/watch?v=mzPxo7Y6JyA',
-                title: "What is HTML5?",
-                thumbnail: 'http://i.ytimg.com/vi/mzPxo7Y6JyA/hqdefault.jpg',
-                locale: 'en',
-                courseUUID : 'html5-css3'
-              },
-               { id: 'ZXkwxeeUU7U',
-                url: 'http://www.youtube.com/watch?v=ZXkwxeeUU7U',
-                title: "Spring Source Core Spring Training",
-                thumbnail: 'http://i.ytimg.com/vi/ZXkwxeeUU7U/hqdefault.jpg',
-                locale: 'en',
-                courseUUID : 'core-spring'
-              }],
-    address : "Madrid, Spain",
-    geopoint : { lat : 40.4166909, lng: -3.70034540000006, zoom : 10 }
-})
+var adminUser = db.users.findOne({email:admin.email});
 
 db.videos.save(
- { youtubeId: 'RY6dNUL8k6o',
-  instructorId: ObjectId('50a2110550e8722c18000001'),
-                url: 'http://www.youtube.com/watch?v=RY6dNUL8k6o',
-                title: "Practical Tips and Tricks with Spring Integration",
-                thumbnail: 'http://i.ytimg.com/vi/RY6dNUL8k6o/hqdefault.jpg',
-                locale: 'en',
-                courseUUID : 'eis',
-                ranking: {
-                  numLikes: 300,
-                  numDislikes: 200,
-                  value: 200
-                }
-              }
-)
+  { youtubeId: 'RY6dNUL8k6o',
+    instructorId: adminUser._id,
+    instructorName: adminUser.name,
+    url: 'http://www.youtube.com/watch?v=RY6dNUL8k6o',
+    title: "Practical Tips and Tricks with Spring Integration",
+    thumbnail: 'http://i.ytimg.com/vi/RY6dNUL8k6o/hqdefault.jpg',
+    locale: 'en',
+    courseUUID : 'eis',
+    ranking: {
+      numLikes: 300,
+      numDislikes: 200,
+      value: 200
+    }
+  }
+);
 
 db.videos.save(
-              { youtubeId: 'TIuI6sjmP7g',
-                instructorId: ObjectId('50a2110550e8722c18000001'),
-                url: 'http://www.youtube.com/watch?v=TIuI6sjmP7g',
-                title: "Intertech - Complete Spring Core Training - Part 3",
-                thumbnail: 'http://i.ytimg.com/vi/TIuI6sjmP7g/hqdefault.jpg',
-                locale: 'en',
-                courseUUID : 'core-spring',
-                ranking: {
-                  numLikes: 500,
-                  numDislikes: 300,
-                  value: 100
-                }
-              }
-)
+  { youtubeId: 'TIuI6sjmP7g',
+    instructorId: adminUser._id,
+    instructorName: adminUser.name,
+    url: 'http://www.youtube.com/watch?v=TIuI6sjmP7g',
+    title: "Intertech - Complete Spring Core Training - Part 3",
+    thumbnail: 'http://i.ytimg.com/vi/TIuI6sjmP7g/hqdefault.jpg',
+    locale: 'en',
+    courseUUID : 'core-spring',
+    ranking: {
+      numLikes: 500,
+      numDislikes: 300,
+      value: 100
+    }
+  }
+);
 
 db.videos.save(
-              { youtubeId: 'OwN6UdDzZNA',
-                instructorId: ObjectId('50a2110550e8722c18000001'),
-                url: 'http://www.youtube.com/watch?v=OwN6UdDzZNA',
-                title: "Spring Tutorial - Using Spring Web Flow (Part 1 of 2)",
-                thumbnail: 'http://i.ytimg.com/vi/OwN6UdDzZNA/hqdefault.jpg',
-                locale: 'en',
-                courseUUID : 'spring-web',
-                ranking: {
-                  numLikes: 700,
-                  numDislikes: 300,
-                  value: 100
-                }
-              }
-)
+  { youtubeId: 'OwN6UdDzZNA',
+    instructorId: adminUser._id,
+    instructorName: adminUser.name,
+    url: 'http://www.youtube.com/watch?v=OwN6UdDzZNA',
+    title: "Spring Tutorial - Using Spring Web Flow (Part 1 of 2)",
+    thumbnail: 'http://i.ytimg.com/vi/OwN6UdDzZNA/hqdefault.jpg',
+    locale: 'en',
+    courseUUID : 'spring-web',
+    ranking: {
+      numLikes: 700,
+      numDislikes: 300,
+      value: 100
+    }
+  }
+);

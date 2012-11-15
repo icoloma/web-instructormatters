3b688c87ae735d93966a35e7197b409ec1e852e3
José Roberto Vidal <rvidal@extrema-sistemas.com>
2012-11-15 19:08:36 +0100

    Script de mongo mejorado: random users y random videos

---
+++ test/db/mongodb-init-data.js
@@ -0,0 +1,94 @@
+var adminUsers = [
+  {
+    email: "ehdez73@gmail.com",
+    name: "Ernesto Hernández",
+    googleId : "116308562574216710328"
+  },
+  {
+    email: "rvidal@extrema-sistemas.com",
+    name: "José Roberto Vidal",
+    googleId: ""
+  }
+],
+regularUsers = [
+  {
+    email: "j.roberto.vidal@gmail.com",
+    name: "José Roberto Vidal",
+    googleId: ""
+  }
+]
+
+var randomInt = function (max) {
+  return Math.floor(Math.random() * max);
+}
+
+var users = [],
+  courses =  [
+    {
+      uuid: 'spring-web',
+      name: 'Spring Web 3.1',
+      duration: '4 days',
+      deleted: false,
+      description: 'Rich web applications with spring Soufflé cotton candy sesame snaps liquorice toffee tootsie roll. Danish caramels dessert tart. Tiramisu fruitcake macaroon jelly. Chocolate bar cookie ice cream bear claw powder sweet roll liquorice cheesecake. Topping dessert sugar plum. Cupcake chocolate cake apple pie marzipan marshmallow dessert. '
+    },
+    {
+      uuid: 'html5-css3',
+      name: 'HTML5 & CSS3',
+      duration: '4 days',
+      deleted: false,
+      description: 'All about HTML5 and CSS3. Jelly beans chocolate bar chocolate cake toffee fruitcake tiramisu toffee bonbon. Pudding powder pastry powder cotton candy danish. Halvah gummies chocolate tiramisu marshmallow macaroon chocolate liquorice donut. Icing icing cupcake chocolate tiramisu jelly beans tootsie roll candy. Halvah candy cookie pastry faworki caramels macaroon lemon drops tart. Croissant sweet roll apple pie. Donut tootsie roll soufflé. Sweet roll candy toffee liquorice faworki.'
+    },
+    {
+      uuid: 'core-spring',
+      name: 'Core Spring',
+      duration: '4 days',
+      deleted: false,
+      description: 'Spring framework 101. The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother\'s keeper and the finder of lost children.'
+    },
+    {
+      uuid: 'eis',
+      name: 'Enterprise Integration with Spring.',
+      duration: '4 days',
+      deleted: false,
+      description: 'Learn all about Enterprise Integration with Spring framework.  Look, just because I don\'t be givin\' no man a foot massage don\'t make it right for Marsellus to throw Antwone into a glass motherfuckin\' house, fuckin\' up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, \'cause I\'ll kill the motherfucker, know what I\'m sayin\'?'
+    },
+],
+  places = [
+    { "address" : "Venezuela", "geopoint" : { "lat" : 6.42375, "lng" : -66.58973000000003 } },
+    { "address" : "Usera, Madrid, Spain", "geopoint" : { "lat" : 40.387097, "lng" : -3.7068950000000314 } },
+    { "address" : "Alcorcón, Spain", "geopoint" : { "lat" : 40.3491158, "lng" : -3.8288109000000077 } },
+    { "address" : "Valencia, Spain", "geopoint" : { "lat" : 39.4702393, "lng" : -0.37680490000002465 } },
+    { "address" : "Embajadores, Madrid, Spain", "geopoint" : { "lat" : 40.4004054, "lng" : -3.6952896000000237 } },
+    { "address" : "Rivas-Vaciamadrid, Spain", "geopoint" : { "lat" : 40.3296966, "lng" : -3.516941299999985 } },
+    { "address" : "Majadahonda, Spain,", "geopoint" : { "lat" : 40.4728332, "lng" : -3.872304399999962 }, },
+    { "address" : "Tenerife, Santa Cruz de Tenerife, Spain", geopoint : { lat : 28.2915637, lng: -16.629130400000008, zoom : 9 } },
+], videos = [
+  { youtubeId : "V7qnG5rBfO0" },
+  { youtubeId : "vMYibbzJlVs" },
+  { youtubeId : "rc87EmY5A08" },
+  { youtubeId : "vMYibbzJlVs" },
+  { youtubeId : "QQul2_fNbhs" },
+  { youtubeId : "_2L8L4w7lvE" },
+  { youtubeId : "Im1mZokp9go" },
+  { youtubeId : "KG5zIouJg-k" },
+  { youtubeId : "LmP1EmUUqgU" },
+  { youtubeId : "fW8amMCVAJQ" },
+  { youtubeId : "ye25XBGhF0A" },
+  { youtubeId : "UErR7i2onW0" },
+  { youtubeId : "NYbTNFN3NBo" },
+  { youtubeId : "amCxbVG8QUs" },
+  { youtubeId : "P4cj1t7v5L4" },
+  { youtubeId : "gJYn4I-OGJU" },
+  { youtubeId : "Yq5hZQ-pM9k" },
+  { youtubeId : "ncPQbKBBMgY" },
+  { youtubeId : "yqlcsWK7EA8" },
+  { youtubeId : "QuG3qrgM4AM" },
+  { youtubeId : "5NtBYdgXfJY" },
+  { youtubeId : "l797bjL3hKA" },
+  { youtubeId : "7ZoSaYZCK8E" },
+  { youtubeId : "LdjPmqXXTCU" },
+].map(function (video) {
+  video.url = "http://www.youtube.com/watch?v=" + video.youtubeId;
+  return video;
+}),
+  states = ["NEW", "NEW", "NEW", "NEW", "NEW", "PENDING", "PAID"];
---
+++ test/db/mongodb-init.js
@@ -0,0 +1,160 @@
+var numCourses = courses.length, numPlaces = places.length, numStates = states.length;
+
+// use instructormatters;
+var db = connect('localhost/instructormatters');
+db.certificates.drop();
+db.editions.drop();
+db.videos.drop();
+db.users.drop();
+db.courses.drop();
+
+//
+//Create users
+//
+
+var extraUsers = 50;
+
+var randomizeUser = function (user) {
+  var excludedCourse = randomInt(numCourses),
+    excludedCert = randomInt(numCourses - 1),
+    chosenCourses = [],
+    chosenCerts = []
+    chosenLocation = places[randomInt(numPlaces)];
+
+  courses.forEach(function (course, i) {
+    if(i !== excludedCourse)
+      chosenCourses.push(course.uuid);
+  });
+  // print(i)
+  chosenCourses.forEach(function (course, i) {
+    if(i !== excludedCert)
+      chosenCerts.push(course);
+  });
+
+  Object.extend(user, {
+    deleted: false,
+    admin: false,
+    aboutMe: Math.random().toString(36) + " " +  Math.random().toString(36),
+    courses: chosenCourses,
+    certificates: chosenCerts,
+    address: chosenLocation.address,
+    geopoint: chosenLocation.geopoint,
+
+  });
+  return user;
+}
+
+//Create admin users
+adminUsers.forEach(function (user) {
+  user = randomizeUser(user);
+  user.admin = true;
+  users.push(user);
+  db.users.save(user);
+});
+
+//Create regular users
+regularUsers.forEach(function (user) {
+  user = randomizeUser(user);
+  users.push(user);
+  db.users.save(user);
+});
+
+//Create random users
+for(var i = 0; i < extraUsers; i++) {
+  var extraUser = {
+    name: Math.random().toString(36).substring(6),
+    email: Math.random().toString(36).substring(6) + "@" +
+            Math.random().toString(36).substring(6) + "." + Math.random().toString(36).substring(3)
+  };
+  extraUser = randomizeUser(extraUser);
+  users.push(extraUser);
+  db.users.save(extraUser);
+}
+
+//Recover user IDs
+users.forEach(function (user) {
+  user._id = db.users.findOne({email: user.email}, {_id: true})._id;
+})
+
+// Create courses
+courses.forEach(function (course) {
+  db.courses.save(course);
+})
+
+//Create random editions
+var numEditions = 60,
+  editions = [],
+  numUsers = users.length,
+  oneCourse, isCertified, oneUser, onePlace, oneDate,
+  now = new Date();
+
+for(var i = 0; i < numEditions; i++) {
+  oneCourse = courses[randomInt(numCourses)],
+  onePlace = places[randomInt(numPlaces)],
+  oneDate = new Date(now.getTime() + Math.round(Math.random() * 1000000000)),
+  isCertified = false;
+
+  while(!isCertified) {
+    oneUser = users[randomInt(numUsers)];
+    if(oneUser.certificates.indexOf(oneCourse.uuid) > -1) {
+      isCertified = true;
+    }
+  }
+
+
+  db.editions.save({
+    deleted: false,
+    address: onePlace.address,
+    geopoint: onePlace.geopoint,
+    instructorId: oneUser._id,
+    instructorName: oneUser.name,
+    state: states[Math.round(Math.random() * numStates)],
+    courseUUID: oneCourse.uuid,
+    date: oneDate
+  })
+}
+
+//Create videos
+var getVideo = function () {
+  if(videos.length > 0) {
+    return videos.splice(randomInt(numVideos), 1)[0];
+  } else {
+    var yId = Math.random().toString(36).substring(6);
+    return {
+      youtubeId: yId,
+      url: "http://www.youtube.com/watch?v=" + yId
+    }
+  }
+};
+
+var locales = ["en", "es"], numVideos = videos.length, oneVideo;
+
+for(var i = 0; i < numUsers; i++) {
+  oneUser = users[i];
+
+  for(var j = 0; j < 3; j ++) {
+    oneVideo = getVideo(),
+    numVideos = videos.length;
+
+    var likes = randomInt(1000),
+      dislikes = randomInt(likes);
+
+    Object.extend(oneVideo, {
+      title: Math.random().toString(36).substring(6),
+      locale: locales[randomInt(2)],
+      instructorId: oneUser._id,
+      instructorName: oneUser.name,
+      courseUUID: courses[randomInt(numCourses)].uuid,
+      ranking: {
+        numLikes: likes,
+        numDislikes: dislikes,
+        value: likes - dislikes
+      },
+      duration: randomInt(180),
+      thumbnail: "http://i.ytimg.com/vi/" + oneVideo.youtubeId + "/mqdefault.jpg"
+    });
+
+
+    db.videos.save(oneVideo);
+  }
+}

fdca0bb9b9f9bd4b5aa33ffa7f8a224fcaa0c1c7
Ernesto Hernandez <ehdez@extrema-sistemas.com>
2012-11-21 18:10:51 +0100

    Comprobamos el domino

--- app.js
+++ app.js
@@ -30,10 +30,10 @@ app.configure(function() {
   app.use(express.cookieParser()); 
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(security.checkDomain);
   app.use(express.session({ secret: 'keyboard cat' }));
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(security.checkDomain);
   app.use(security.exposeCurrentUser);
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
